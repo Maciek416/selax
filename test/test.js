6017b544fbbcd4bc652ca5bb2a58a040fec62fe5
Maciej Adwent <maciekadwent@gmail.com>
2013-11-10 17:04:36 -0800

    Parse text nodes, CDATA. Support text() call

--- test/test.js
+++ test/test.js
@@ -7,19 +7,25 @@ var should = require('should');
 var fs = require('fs');
 var Selax = require('../index');
 
+var getXML = function(test){
+  fs.readFile('./test/nasa.rss', function (errorMessage, xml) {
+    if (errorMessage) {
+      console.log(errorMessage);
+      return;
+    }
+    xml = xml.toString();
+
+    test(xml);
+  });
+};
+
 describe('Selax', function() {
 
-  describe('parsing and matching', function(){
+  describe('matching', function(){
 
     it('should find 10 item nodes in the XML file', function(done) {
-
-      fs.readFile('./test/nasa.rss', function (errorMessage, xml) {
-        if (errorMessage) {
-          console.log(errorMessage);
-          return;
-        }
-        xml = xml.toString();
-
+      
+      getXML(function(xml){
         var matched = [];
         var ax = new Selax('item');
 
@@ -36,5 +42,20 @@ describe('Selax', function() {
       });
 
     });
+
+    it('should parse text correctly', function(done) {
+      getXML(function(xml){
+        var ax = new Selax('channel > description');
+
+        ax.on('match', function(node){
+          node.text().should.eql('A RSS news feed containing the latest NASA news articles and press releases.');
+          done();
+        });
+
+        ax.write(xml);
+      });
+    });
+
   });
+
 });
