a5729b0409c87e7a6250d77c92aff9a47a22be10
Maciej Adwent <maciekadwent@gmail.com>
2014-01-19 17:07:25 -0800

    Selax is now a Readable instead of an emitter

--- README.md
+++ README.md
@@ -5,20 +5,29 @@ Selax
 
 A library for streaming XML nodes matched with CSS3 selectors.
 
+Selax implements node's `stream.Transform`, which means you can use it to `pipe()` data directly from a source like a file read stream, as well as listen to events like `readable` and `end`.
+
 ```javascript
 
+// Stream temperature nodes from a met.no RSS weather data document (see: http://api.met.no/weatherapi/documentation )
+
+var source = fs.createReadStream('weatherdata.rss');
 var Selax = require('selax');
 
 var ax = new Selax('weatherdata time:first-child temperature');
 
-ax.on('match', function(node){
-  console.log('Found matching node: ', node);
+source.pipe(ax);
+
+ax.on('readable', function(){
+  var node;
+
+  while(node = ax.read()){
+    console.log('Found matching temperature node: ', node);
+  }
 });
 
 ax.on('end', function(){
-  console.log('Finished selecting nodes');
+  console.log('Finished selecting temperature nodes');
 });
-
-ax.write(xmlString);
 
 ```
--- test/test.js
+++ test/test.js
@@ -6,53 +6,44 @@ var should = require('should');
 
 var fs = require('fs');
 var Selax = require('../index');
-
-var getXML = function(test){
-  fs.readFile('./test/nasa.rss', function (errorMessage, xml) {
-    if (errorMessage) {
-      console.log(errorMessage);
-      return;
-    }
-    xml = xml.toString();
-
-    test(xml);
-  });
-};
+var filename = './test/nasa.rss';
 
 describe('Selax', function() {
 
-  describe('matching', function(){
+  describe('matching', function() {
 
     it('should find 10 item nodes in the XML file', function(done) {
-      
-      getXML(function(xml){
-        var matched = [];
-        var ax = new Selax('item');
 
-        ax.on('match', function(node){
-          matched.push(node);
-        });
+      var matched = [];
+      var ax = new Selax('item');
+      var source = fs.createReadStream(filename);
 
-        ax.on('end', function(){
-          matched.length.should.eql(10);
-          done();
-        });
+      source.pipe(ax);
 
-        ax.write(xml);
+      ax.on('readable', function() {
+        var item;
+        while(item = ax.read()){
+          matched.push(item);
+        }
       });
 
+      ax.on('end', function() {
+        matched.length.should.eql(10);
+        done();
+      });
+      
     });
 
     it('should parse text correctly', function(done) {
-      getXML(function(xml){
-        var ax = new Selax('channel > description');
+      var ax = new Selax('channel > description');
+      var source = fs.createReadStream(filename);
 
-        ax.on('match', function(node){
-          node.text().should.eql('A RSS news feed containing the latest NASA news articles and press releases.');
-          done();
-        });
+      source.pipe(ax);
 
-        ax.write(xml);
+      ax.on('data', function(node){
+        node.text().should.eql('A RSS news feed containing the latest NASA news articles and press releases.');
+        ax.end();
+        done();
       });
     });
 
